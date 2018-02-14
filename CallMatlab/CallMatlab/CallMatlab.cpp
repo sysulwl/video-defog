24c92a507592f2e86c1b2e321a84aa7403245a0c
liuwanli <547058465@qq.com>
2018-02-14 17:55:05 +0800

    参考网友的代码，可以C++调用matlab引擎来画图了。

--- CallMatlab/CallMatlab/CallMatlab.cpp
+++ CallMatlab/CallMatlab/CallMatlab.cpp
@@ -47,12 +47,45 @@ int APIENTRY _tWinMain(HINSTANCE hInstance,		//main function
 
 	hAccelTable = LoadAccelerators(hInstance, MAKEINTRESOURCE(IDC_CALLMATLAB));
 
-	Engine *ep;
-	ep = engOpen(""); //定义Matlab引擎指针。初始化 
-	if ((ep = engOpen("\0")) == NULL)  
-	{  
-		return EXIT_FAILURE;  
-	} 
+		    Engine *ep;  
+  
+    // open engine  
+    if ((ep = engOpen("\0")) == NULL)  
+    {  
+        return EXIT_FAILURE;  
+    }  
+  
+    // generate variables  
+    int Nsample = 50;  
+    const double PI = 3.1415926;  
+    double *t = new double[Nsample] ;  
+  
+    for(int i = 0; i < Nsample; i++)  
+    {  
+        t[i] = i * 2 * PI / Nsample;  
+    }  
+  
+    mxArray *T = NULL;  
+    T = mxCreateDoubleMatrix(1, Nsample, mxREAL);  //1. 行数, 2. 列数  3.实数
+    memcpy((void *)mxGetPr(T), (void *)t, Nsample*sizeof(t[0])); //copy内容 
+  
+    engPutVariable(ep, "T", T);         // put data to engine  
+  
+    // execute matlab operations  
+    engEvalString(ep, "Y=sin(T);");  
+    engEvalString(ep, "plot(T,Y);");  
+    engEvalString(ep, "title('y=sin(t)');");  
+    engEvalString(ep, "xlabel('t');");  
+    engEvalString(ep, "ylabel('y');");  
+  
+    // clean operation(don't forget!!!)  
+    mxDestroyArray(T); 
+	T = NULL;
+    engEvalString(ep, "close;");  
+  
+    // close engine  
+    engClose(ep);  
+	ep = NULL;
 
 	// 主消息循环:
 	while (GetMessage(&msg, NULL, 0, 0))
