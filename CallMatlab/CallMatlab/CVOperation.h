7240a01e2729e32999edc07966956d3e871fd5e3
liuwanli <547058465@qq.com>
2018-03-10 20:48:00 +0800

    同上，忘了提交

--- CallMatlab/CallMatlab/CVOperation.h
+++ CallMatlab/CallMatlab/CVOperation.h
@@ -1,6 +1,7 @@
-#include <vector>  
+﻿#include <vector>  
 #include <stdio.h>  
 #include<opencv2/opencv.hpp>  
+#include<opencv2/core/core.hpp>  
 using namespace std;
 using namespace cv;
 
@@ -38,5 +39,39 @@ int CreatePNG()
 		return 1;  
 	}  
 	//fprintf(stdout,"PNG图片文件的alpha数据保存完毕~\n");  
+	return 0;  
+}  
+
+int UseExample( )  
+{  
+	Mat image= imread("C:\\Users\\lwl\\Desktop\\dota.jpg");  
+	Mat logo= imread("C:\\Users\\lwl\\Desktop\\dota_logo.jpg");  
+
+	//载入后先显示  
+	namedWindow("【2】原画图");  
+	imshow("【2】原画图",image);  
+	cv::waitKey(6000);
+	namedWindow("【3】logo图");  
+	imshow("【3】logo图",logo);  
+	cv::waitKey(6000);
+	//定义一个Mat类型，用于存放，图像的ROI  
+	cv::Mat imageROI;
+	//方法一  
+	imageROI=image(cv::Rect(201,200,logo.cols,logo.rows));  
+	//将logo加到原图上  
+	addWeighted(imageROI,0.5,logo,0.3,0.0,imageROI);
+
+	//方法二  
+	//imageROI=image(Range(350,350+logo.rows),Range(800,800+logo.cols));  
+	//显示结果  
+	namedWindow("【4】原画+logo图");  
+	imshow("【4】原画+logo图",image);  
+
+	//-----------------------------------【三、图像的输出】--------------------------------------  
+	//     描述：将一个Mat图像输出到图像文件  
+	//-----------------------------------------------------------------------------------------------  
+	//输出一张jpg图片到工程目录下  
+	imwrite("C:\\Users\\lwl\\Desktop\\混合.jpg",image);  
+	waitKey();  
 	return 0;  
 }  
