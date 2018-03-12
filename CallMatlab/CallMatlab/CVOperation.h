e9f3fbbc38ce3c76602e0111653b8f9ae77c54ef
liuwanli <547058465@qq.com>
2018-03-12 22:28:34 +0800

    方框滤波的样例

--- CallMatlab/CallMatlab/CVOperation.h
+++ CallMatlab/CallMatlab/CVOperation.h
@@ -289,3 +289,25 @@ int ContrastAndBrightExample(  )
 	while(char(waitKey(1)) != 'q') {}  
 	return 0;  
 }  
+
+void BoxFilter()		//方框滤波
+{  
+	//载入原图  
+	Mat image=imread("2.jpg");  
+
+	//创建窗口  
+	namedWindow("均值滤波【原图】" );  
+	namedWindow("均值滤波【效果图】");  
+
+	//显示原图  
+	imshow("均值滤波【原图】", image );  
+
+	//进行滤波操作  
+	Mat out;  
+	boxFilter(image, out, -1,cv::Size(5, 5));  
+
+	//显示效果图  
+	imshow("均值滤波【效果图】" ,out );  
+
+	waitKey(0 );      
+}  
