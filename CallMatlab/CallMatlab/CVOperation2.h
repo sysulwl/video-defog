d7222cfd6d908659d5682b343f7f9cbcc40972dd
liuwanli <547058465@qq.com>
2018-03-12 23:01:23 +0800

    非线性滤波器: 中值滤波器

--- CallMatlab/CallMatlab/CVOperation2.h
+++ CallMatlab/CallMatlab/CVOperation2.h
@@ -115,3 +115,23 @@ static void on_GaussianBlur(int, void *)
 	//显示窗口  
 	imshow("【<3>高斯滤波】", g_dstImage3);  
 } 
+
+int MedianBlur( )  
+{  
+	//载入原图  
+	Mat image=imread("1.jpg");  
+
+	//创建窗口  
+	namedWindow("中值滤波【原图】" );  
+	namedWindow("中值滤波【效果图】");  
+
+	//显示原图  
+	imshow("中值滤波【原图】", image );  
+
+	//进行中值滤波操作  
+	Mat out;  
+	medianBlur( image, out, 7);  
+	//显示效果图  
+	imshow("中值滤波【效果图】" ,out );  
+	waitKey(0 );      
+} 
