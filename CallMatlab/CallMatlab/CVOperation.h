89c5358698f75de55d9ddab4a2d0189426e3e384
liuwanli <547058465@qq.com>
2018-03-12 22:32:07 +0800

    均值滤波——变模糊，尤其是细节，边缘没有那么明显了

--- CallMatlab/CallMatlab/CVOperation.h
+++ CallMatlab/CallMatlab/CVOperation.h
@@ -311,3 +311,25 @@ void BoxFilter()		//方框滤波
 
 	waitKey(0 );      
 }  
+
+void Blur( )	//均值滤波
+{  
+	//载入原图  
+	Mat image=imread("1.jpg");  
+
+	//创建窗口  
+	namedWindow("均值滤波【原图】" );  
+	namedWindow("均值滤波【效果图】");  
+
+	//显示原图  
+	imshow("均值滤波【原图】", image );  
+
+	//进行滤波操作  
+	Mat out;  
+	blur(image, out, cv::Size(7, 7));  
+
+	//显示效果图  
+	imshow("均值滤波【效果图】" ,out );  
+
+	waitKey(0 );      
+} 
